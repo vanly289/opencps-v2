1bf074954820148d4cede59666b566c7d981071e
Đặng Khánh Trung <trung.dk070384@gmail.com>
2018-06-04 12:01:44 +0700

    Add 36-44 result message

--- modules/backend-thirdparty-system/backend-thirdparty-system-service/src/main/java/org/opencps/thirdparty/system/scheduler/BGTVT0600036.java
+++ modules/backend-thirdparty-system/backend-thirdparty-system-service/src/main/java/org/opencps/thirdparty/system/scheduler/BGTVT0600036.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.opencps.auth.utils.APIDateTimeUtils;
+import org.opencps.datamgt.utils.DateTimeUtils;
 import org.opencps.dossiermgt.model.Dossier;
 import org.opencps.dossiermgt.model.DossierAction;
 import org.opencps.dossiermgt.model.DossierFile;
@@ -36,9 +37,12 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 
 public class BGTVT0600036 {
+	private static String DUMMY_DATA = "1";
+	
 	public static MessageQueueInputModel convertResult(Dossier dossier, ThirdPartyDossierSync dossierSync, Envelope envelope, String type, String function) throws PortalException {
 		long dossierActionId = dossierSync.getMethod() == 0 ? dossierSync.getClassPK() : 0;
 		String jaxRsPublicUrl = PrefsPropsUtil.getString(SyncServerTerm.JAXRS_PUBLIC_URL);
@@ -135,14 +139,221 @@ public class BGTVT0600036 {
 					
 					vtOfficialTransportPermit.setNameOfCompany(formDataObj.getString("NameOfCompany"));
 					vtOfficialTransportPermit.setAddress(formDataObj.getString("Address"));
+	
+					if (formDataObj.has("ContactName")) {
+						vtOfficialTransportPermit.setContactName(formDataObj.getString("ContactName"));						
+					}
+					else {
+						vtOfficialTransportPermit.setContactName(StringPool.BLANK);												
+					}
 					
+					if (formDataObj.has("RegistrationNumber")) {
+						vtOfficialTransportPermit.setRegistrationNumber(formDataObj.getString("RegistrationNumber"));						
+					}
+					else {
+						vtOfficialTransportPermit.setRegistrationNumber(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("Passenger")) {
+						vtOfficialTransportPermit.setPassenger(formDataObj.getString("Passenger"));						
+					}
+					else {
+						vtOfficialTransportPermit.setPassenger(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("Capacity")) {
+						vtOfficialTransportPermit.setCapacity(formDataObj.getString("Capacity"));						
+					}
+					else {
+						vtOfficialTransportPermit.setCapacity(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("TrademarkCode")) {
+						vtOfficialTransportPermit.setTrademarkCode(formDataObj.getString("TrademarkCode"));						
+					}
+					else {
+						vtOfficialTransportPermit.setTrademarkCode(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("TrademarkName")) {
+						vtOfficialTransportPermit.setTrademarkName(formDataObj.getString("TrademarkName"));						
+					}
+					else {
+						vtOfficialTransportPermit.setTrademarkName(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("VehicleColor")) {
+						vtOfficialTransportPermit.setVehicleColor(formDataObj.getString("VehicleColor"));						
+					}
+					else {
+						vtOfficialTransportPermit.setVehicleColor(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("EngineNumber")) {
+						vtOfficialTransportPermit.setEngineNumber(formDataObj.getString("EngineNumber"));						
+					}
+					else {
+						vtOfficialTransportPermit.setEngineNumber(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("ChassisNumber")) {
+						vtOfficialTransportPermit.setChassisNumber(formDataObj.getString("ChassisNumber"));						
+					}
+					else {
+						vtOfficialTransportPermit.setChassisNumber(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("Purpose")) {
+						vtOfficialTransportPermit.setPurpose(formDataObj.getString("Purpose"));						
+					}
+					else {
+						vtOfficialTransportPermit.setPurpose(StringPool.BLANK);												
+					}
+
+					if (formDataObj.has("NameOfCompany")) {
+						vtOfficialTransportPermit.setNameOfCompany(formDataObj.getString("NameOfCompany"));						
+					}
+					else {
+						vtOfficialTransportPermit.setNameOfCompany(StringPool.BLANK);												
+					}
+					if (formDataObj.has("DepartureFrom")) {
+						vtOfficialTransportPermit.setDepartureFrom(formDataObj.getString("DepartureFrom"));						
+					}
+					else {
+						vtOfficialTransportPermit.setDepartureFrom(StringPool.BLANK);												
+					}
+					if (formDataObj.has("DepartureTo")) {
+						vtOfficialTransportPermit.setDepartureTo(formDataObj.getString("DepartureTo"));						
+					}
+					else {
+						vtOfficialTransportPermit.setDepartureTo(StringPool.BLANK);												
+					}
+					if (formDataObj.has("FromParkingLot")) {
+						vtOfficialTransportPermit.setFromParkingLot(formDataObj.getString("FromParkingLot"));						
+					}
+					else {
+						vtOfficialTransportPermit.setFromParkingLot(StringPool.BLANK);												
+					}
+					if (formDataObj.has("FromProvinceCode")) {
+						vtOfficialTransportPermit.setFromProvinceCode(formDataObj.getString("FromProvinceCode"));						
+					}
+					else {
+						vtOfficialTransportPermit.setFromProvinceCode(DUMMY_DATA);												
+					}
+					if (formDataObj.has("FromProvinceName")) {
+						vtOfficialTransportPermit.setFromProvinceName(formDataObj.getString("FromProvinceName"));						
+					}
+					else {
+						vtOfficialTransportPermit.setFromProvinceName(DUMMY_DATA);												
+					}
+					if (formDataObj.has("ToParkingLot")) {
+						vtOfficialTransportPermit.setToParkingLot(formDataObj.getString("ToParkingLot"));						
+					}
+					else {
+						vtOfficialTransportPermit.setToParkingLot(DUMMY_DATA);												
+					}
+					if (formDataObj.has("ToProvinceCode")) {
+						vtOfficialTransportPermit.setToProvinceCode(formDataObj.getString("ToProvinceCode"));						
+					}
+					else {
+						vtOfficialTransportPermit.setToProvinceCode(DUMMY_DATA);												
+					}
+					if (formDataObj.has("ToProvinceName")) {
+						vtOfficialTransportPermit.setToProvinceName(formDataObj.getString("ToProvinceName"));						
+					}
+					else {
+						vtOfficialTransportPermit.setToProvinceName(DUMMY_DATA);												
+					}
+					if (formDataObj.has("Routes")) {
+						vtOfficialTransportPermit.setRoutes(formDataObj.getString("Routes"));						
+					}
+					else {
+						vtOfficialTransportPermit.setRoutes(StringPool.BLANK);												
+					}
+					if (formDataObj.has("GoodsType")) {
+						vtOfficialTransportPermit.setGoodsType(formDataObj.getString("GoodsType"));						
+					}
+					else {
+						vtOfficialTransportPermit.setGoodsType(StringPool.BLANK);												
+					}
+					if (formDataObj.has("Stops")) {
+						vtOfficialTransportPermit.setStops(formDataObj.getString("Stops"));						
+					}
+					else {
+						vtOfficialTransportPermit.setStops(DUMMY_DATA);												
+					}
+					if (formDataObj.has("LicenceType")) {
+						vtOfficialTransportPermit.setLicenceType(formDataObj.getString("LicenceType"));						
+					}
+					else {
+						vtOfficialTransportPermit.setLicenceType(DUMMY_DATA);												
+					}
+					if (formDataObj.has("LicenceNo")) {
+						vtOfficialTransportPermit.setLicenceNo(formDataObj.getString("LicenceNo"));						
+					}
+					else {
+						vtOfficialTransportPermit.setLicenceNo(DUMMY_DATA);												
+					}
+					if (formDataObj.has("ValidFrom")) {
+						String validFromStr = formDataObj.getString("ValidFrom");
+						Date validFromDate = DateTimeUtils.convertStringToDate(validFromStr);
+
+						vtOfficialTransportPermit.setValidFrom(
+								DateTimeUtils.convertDateToString(validFromDate, DateTimeUtils._NSW_DATE_TIME_FORMAT)
+								);						
+
+					}
+					else {
+						Date now = new Date();
+						vtOfficialTransportPermit.setValidFrom(DateTimeUtils.convertDateToString(now, DateTimeUtils._NSW_DATE_TIME_FORMAT));												
+					}
+
+					if (formDataObj.has("ValidTo")) {
+						String validToStr = formDataObj.getString("ValidTo");
+						Date validToDate = DateTimeUtils.convertStringToDate(validToStr);
+
+						vtOfficialTransportPermit.setValidUntil(
+								DateTimeUtils.convertDateToString(validToDate, DateTimeUtils._NSW_DATE_TIME_FORMAT)
+								);						
+
+					}
+					else {
+						Date now = new Date();
+						vtOfficialTransportPermit.setValidUntil(DateTimeUtils.convertDateToString(now, DateTimeUtils._NSW_DATE_TIME_FORMAT));												
+					}
+
 					IssuingAuthority issuingAuthority = new IssuingAuthority();
-					issuingAuthority.setSignName(formDataObj.getString("SignName"));
-					issuingAuthority.setSignDate(formDataObj.getString("SignDate"));
-					issuingAuthority.setSignPlace(formDataObj.getString("SignPlace"));
-					issuingAuthority.setSignTitle(formDataObj.getString("SignTitle"));
+					Date now = new Date();
 					
-					vtOfficialTransportPermit.setIssuingAuthority(issuingAuthority);
+					if (formDataObj.has("SignName")) {
+						issuingAuthority.setSignName(formDataObj.getString("SignName"));						
+					}
+					else {
+						issuingAuthority.setSignName("Nguyễn Tô An");						
+					}
+					if (formDataObj.has("SignDate")) {
+						String signDateStr = formDataObj.getString("SignDate");
+						Date signDate = DateTimeUtils.convertStringToDate(signDateStr);
+						
+						issuingAuthority.setSignDate(DateTimeUtils.convertDateToString(signDate, DateTimeUtils._NSW_DATE_TIME_FORMAT));						
+					}
+					else {
+						issuingAuthority.setSignDate(DateTimeUtils.convertDateToString(now, DateTimeUtils._NSW_DATE_TIME_FORMAT));						
+					}
+					if (formDataObj.has("SignPlace")) {
+						issuingAuthority.setSignPlace(formDataObj.getString("SignPlace"));						
+					}
+					else {
+						issuingAuthority.setSignPlace("Hà Nội");						
+					}
+					if (formDataObj.has("SignTitle")) {
+						issuingAuthority.setSignTitle(formDataObj.getString("SignTitle"));						
+					}
+					else {
+						issuingAuthority.setSignTitle("Cục trưởng");												
+					}
+					
+					vtOfficialTransportPermit.setIssuingAuthority(issuingAuthority);										
 				}
 			}
 		}
