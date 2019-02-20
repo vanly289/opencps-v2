2027a8c58bc633029996090d995561e1c5b7e3d6
vuhongson <sonvh@fds.vn>
2019-02-20 17:23:40 +0700

    first commit

---
+++ modules/backend-datamgt/backend-datamgt-service/src/main/java/org/opencps/datamgt/service/persistence/impl/DictItemFinderImpl.java
@@ -0,0 +1,301 @@
+package org.opencps.datamgt.service.persistence.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import org.opencps.datamgt.model.DictCollection;
+import org.opencps.datamgt.model.DictGroup;
+import org.opencps.datamgt.model.impl.DictCollectionImpl;
+import org.opencps.datamgt.model.impl.DictCollectionModelImpl;
+import org.opencps.datamgt.service.DictCollectionLocalServiceUtil;
+import org.opencps.datamgt.service.DictGroupLocalServiceUtil;
+import org.opencps.datamgt.service.DictItemGroupLocalServiceUtil;
+import org.opencps.datamgt.service.persistence.DictCollectionPersistence;
+import org.opencps.datamgt.service.persistence.DictItemFinder;
+import org.opencps.datamgt.model.DictItem;
+import org.opencps.datamgt.model.impl.DictItemImpl;
+import org.opencps.datamgt.model.impl.DictItemModelImpl;
+import org.opencps.datamgt.service.persistence.DictItemPersistence;
+import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQLUtil;
+
+public class DictItemFinderImpl extends DictItemFinderBaseImpl implements DictItemFinder {
+	
+	private static Log log = LogFactoryUtil.getLog(DictItemFinderImpl.class);
+	
+	public List<DictItem> findByCollection_Level_OrderBy(long groupId, String collectionCode, String level, String OrderBy ) {
+		Session session = null;
+		try {
+			session = openSession();
+			
+			StringBuilder query = new StringBuilder();
+			query.append("SELECT {opencps_dictitem.*} FROM opencps_dictitem ");
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				// bo qua INNER JOIN parentItem
+			} else {
+				query.append("INNER JOIN opencps_dictitem parent  ");
+				query.append("ON opencps_dictitem.dictCollectionId = parent.dictCollectionId  ");
+				query.append("AND opencps_dictitem.parentItemId = parent.dictItemId  ");
+				if (Validator.isNotNull(level)) {
+					query.append("AND parent.level <   " + level);
+				} else {
+					query.append("AND parent.level = 1  ");
+				}
+			}			
+			
+			query.append("INNER JOIN opencps_dictcollection collection  ");
+			query.append("ON opencps_dictitem.dictCollectionId = collection.dictCollectionId  ");
+			query.append("AND collection.collectionCode in (" + collectionCode + ")  ");
+			query.append("WHERE  1=1 ");
+			if (groupId > 0 ) {
+				query.append(" and opencps_dictitem.groupId =   " + groupId);
+			} else { 
+				query.append(" and opencps_dictitem.groupId = 55301  ");	
+			}
+			
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				query.append(" and opencps_dictitem.level = 0  ");
+			} else {
+				query.append(" and opencps_dictitem.parentItemId > 0  ");
+			}
+			
+			if (Validator.isNotNull(OrderBy) && OrderBy.contains("name")) {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			} else if (Validator.isNotNull(OrderBy) && OrderBy.contains("sibling")) {
+				query.append("order by opencps_dictitem.parentItemId, LENGTH(sibling), sibling ");
+			} else {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			}
+			
+					
+			SQLQuery q = session.createSQLQuery(query.toString());
+			q.setCacheable(false);
+			q.addEntity(DictItemImpl.TABLE_NAME, DictItemImpl.class);
+			
+			log.info("=========findByCollection_Level_OrderBy===" + query.toString() );
+			QueryPos qPos = QueryPos.getInstance(q);
+			
+			
+			return (List<DictItem>) QueryUtil.list(q, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		} catch (Exception e) {
+			log.error(e);
+		} finally {
+			closeSession(session);
+		}
+		
+		return new ArrayList<DictItem>();
+	}
+	
+	public List<DictItem> findByCollection_Parent_Level_OrderBy(long groupId, String collectionCode, String parentItemCode, String level, String OrderBy ) {
+		Session session = null;
+		try {
+			session = openSession();
+			
+			StringBuilder query = new StringBuilder();
+			query.append("SELECT {opencps_dictitem.*} FROM opencps_dictitem ");
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				// bo qua INNER JOIN parentItem
+			} else {
+				query.append("INNER JOIN opencps_dictitem parent  ");
+				query.append("ON opencps_dictitem.dictCollectionId = parent.dictCollectionId  ");
+				query.append("AND opencps_dictitem.parentItemId = parent.dictItemId  ");
+				if (Validator.isNotNull(level)) {
+					query.append("AND parent.level <   " + level);
+				} else {
+					query.append("AND parent.level = 1  ");
+				}				
+				
+				String parentFilter = StringPool.BLANK;
+				if (Validator.isNotNull(parentItemCode)) {
+					String[] arrGroupCode = StringUtil.split(parentItemCode);
+					if (arrGroupCode != null && arrGroupCode.length > 0) {
+						for (int i = 0; i < arrGroupCode.length; i++) {
+							if (Validator.isNotNull(arrGroupCode[i])) {
+								if (Validator.isNotNull(parentFilter)) {
+									parentFilter = parentFilter + " OR  " + "parent.itemCode like '"+arrGroupCode[i].trim()+"%' ";
+								} else {
+									parentFilter = " AND ( "  + "parent.itemCode like '"+arrGroupCode[i].trim()+"%' ";
+								}
+								
+							}
+
+						}
+					}
+				}
+				if (Validator.isNotNull(parentFilter)) {
+					query.append(parentFilter + " )  ");
+				}
+			}			
+			
+			query.append("INNER JOIN opencps_dictcollection collection  ");
+			query.append("ON opencps_dictitem.dictCollectionId = collection.dictCollectionId  ");
+			query.append("AND collection.collectionCode in (" + collectionCode + ")  ");
+			query.append("WHERE  1=1 ");
+			if (groupId > 0 ) {
+				query.append(" and opencps_dictitem.groupId =   " + groupId);
+			} else { 
+				query.append(" and opencps_dictitem.groupId = 55301  ");	
+			}
+			
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				query.append(" and opencps_dictitem.level = 0  ");
+			} else {
+				query.append(" and opencps_dictitem.parentItemId > 0  ");
+			}
+			
+			if (Validator.isNotNull(OrderBy) && OrderBy.contains("name")) {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			} else if (Validator.isNotNull(OrderBy) && OrderBy.contains("sibling")) {
+				query.append("order by opencps_dictitem.parentItemId, LENGTH(sibling), sibling ");
+			} else {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			}
+			
+					
+			SQLQuery q = session.createSQLQuery(query.toString());
+			q.setCacheable(false);
+			q.addEntity(DictItemImpl.TABLE_NAME, DictItemImpl.class);
+			
+			log.info("=========findByCollection_Parent_Level_OrderBy===" + query.toString() );
+			QueryPos qPos = QueryPos.getInstance(q);
+			
+			
+			return (List<DictItem>) QueryUtil.list(q, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		} catch (Exception e) {
+			log.error(e);
+		} finally {
+			closeSession(session);
+		}
+		
+		return new ArrayList<DictItem>();
+	}
+	
+	
+	public List<DictItem> findByCollection_Group_Level_OrderBy(long groupId, String collectionCode, String groupCode , String level, String OrderBy ) {
+		Session session = null;
+		try {
+			session = openSession();
+			
+			StringBuilder query = new StringBuilder();
+			query.append("SELECT {opencps_dictitem.*} FROM opencps_dictitem ");
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				// bo qua INNER JOIN parentItem
+			} else {
+				query.append("INNER JOIN opencps_dictitem parent  ");
+				query.append("ON opencps_dictitem.dictCollectionId = parent.dictCollectionId  ");
+				query.append("AND opencps_dictitem.parentItemId = parent.dictItemId  ");
+				if (Validator.isNotNull(level)) {
+					query.append("AND parent.level <   " + level);
+				} else {
+					query.append("AND parent.level = 1  ");
+				}	
+			}			
+
+			query.append("INNER JOIN opencps_dictcollection collection  ");
+			query.append("ON opencps_dictitem.dictCollectionId = collection.dictCollectionId  ");
+			query.append("AND collection.collectionCode in (" + collectionCode + ")  ");
+			
+			query.append("INNER JOIN opencps_dictgroup dictgroup  ");
+			query.append("ON opencps_dictitem.dictCollectionId = dictgroup.dictCollectionId  ");
+			query.append("INNER JOIN opencps_dictitemgroup itemgroup  ");
+			query.append("ON opencps_dictitem.dictItemId = itemgroup.dictItemId  ");
+			query.append("AND itemgroup.dictGroupId = dictgroup.dictGroupId  ");
+			
+			String groupCodeFilter = StringPool.BLANK;
+			if (Validator.isNotNull(groupCode)) {
+				String[] arrGroupCode = StringUtil.split(groupCode);
+				if (arrGroupCode != null && arrGroupCode.length > 0) {
+					for (int i = 0; i < arrGroupCode.length; i++) {
+						if (Validator.isNotNull(arrGroupCode[i])) {
+							if (Validator.isNotNull(groupCodeFilter)) {
+								groupCodeFilter = groupCodeFilter + " OR  " + "itemgroup.dictGroupName in ("+arrGroupCode[i].trim()+") ";
+							} else {
+								groupCodeFilter = " AND ( "  + "itemgroup.dictGroupName in ("+arrGroupCode[i].trim()+") ";
+							}
+							
+						}
+
+					}
+				}
+			}
+			if (Validator.isNotNull(groupCodeFilter)) {
+				query.append(groupCodeFilter + " )  ");
+			}			
+			
+			query.append("WHERE  1=1 ");
+			if (groupId > 0 ) {
+				query.append(" and opencps_dictitem.groupId =   " + groupId);
+			} else { 
+				query.append(" and opencps_dictitem.groupId = 55301  ");	
+			}
+			
+			if ((Validator.isNotNull(level)) && level.equalsIgnoreCase("0")){
+				query.append(" and opencps_dictitem.level = 0  ");
+			} else {
+				query.append(" and opencps_dictitem.parentItemId > 0  ");
+			}
+			
+			if (Validator.isNotNull(OrderBy) && OrderBy.contains("name")) {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			} else if (Validator.isNotNull(OrderBy) && OrderBy.contains("sibling")) {
+				query.append("order by opencps_dictitem.parentItemId, LENGTH(sibling), sibling ");
+			} else {
+				query.append("order by opencps_dictitem.parentItemId, CONVERT(opencps_dictitem.itemName USING utf8) COLLATE utf8_polish_ci ");
+			}
+			
+					
+			SQLQuery q = session.createSQLQuery(query.toString());
+			q.setCacheable(false);
+			q.addEntity(DictItemImpl.TABLE_NAME, DictItemImpl.class);
+			
+			log.info("=========findByCollection_Parent_Level_OrderBy===" + query.toString() );
+			QueryPos qPos = QueryPos.getInstance(q);
+			
+			
+			return (List<DictItem>) QueryUtil.list(q, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		} catch (Exception e) {
+			log.error(e);
+		} finally {
+			closeSession(session);
+		}
+		
+		return new ArrayList<DictItem>();
+	}
+	
+	/*
+	public List<DictItem> findCustomSLQ_GroupName_AlterName123(String groupName, String alterName) {
+		Session session = null;
+		try {
+			session = openSession();
+			 //get sql query return in default.xml
+            String query=CustomSQLUtil.get(getClass(),"getAllProduct");
+			SQLQuery q = session.createSQLQuery(query.toString());
+			q.setCacheable(false);
+			q.addEntity(DictItemImpl.TABLE_NAME, DictItemImpl.class);
+			
+			log.info("=========findByGroupName_AlterName123===" + query.toString() );
+			QueryPos qPos = QueryPos.getInstance(q);
+			qPos.add(alterName);
+			qPos.add(groupName);
+			
+			return (List<DictItem>) QueryUtil.list(q, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		} catch (Exception e) {
+			log.error(e);
+		} finally {
+			closeSession(session);
+		}
+		
+		return new ArrayList<DictItem>();
+	}	
+	 */
+}
