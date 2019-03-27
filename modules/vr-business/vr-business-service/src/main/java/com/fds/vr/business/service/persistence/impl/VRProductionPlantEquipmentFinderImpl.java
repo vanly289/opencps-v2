141c6aedf1300fe3e7f05640aa5c86d35b837ec9
vuhongson <sonvh@fds.vn>
2019-03-27 17:41:45 +0700

    update finder

---
+++ modules/vr-business/vr-business-service/src/main/java/com/fds/vr/business/service/persistence/impl/VRProductionPlantEquipmentFinderImpl.java
@@ -0,0 +1,37 @@
+package com.fds.vr.business.service.persistence.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.opencps.datamgt.model.DictItem;
+import org.opencps.datamgt.model.impl.DictItemImpl;
+
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.Type;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQLUtil;
+
+import com.fds.vr.business.model.VRProductionPlantEquipment;
+import com.fds.vr.business.model.impl.VRProductionPlantEquipmentImpl;
+import com.fds.vr.business.service.persistence.VRProductionPlantEquipmentFinder;
+
+
+public class VRProductionPlantEquipmentFinderImpl extends VRProductionPlantEquipmentFinderBaseImpl
+	implements VRProductionPlantEquipmentFinder {
+
+	Log _log = LogFactoryUtil.getLog(VRProductionPlantEquipmentFinderImpl.class);
+
+	
+}
