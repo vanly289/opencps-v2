6461794faa6e33ef5961e19e0a08f0466adac380
Nguyen Dung <svtt_dungnv@fds.vn>
2020-10-06 14:39:27 +0700

    update VRApplicantProfile

--- modules/vr-business/vr-business-service/src/main/java/com/fds/vr/business/service/impl/VRApplicantProfileLocalServiceImpl.java
+++ modules/vr-business/vr-business-service/src/main/java/com/fds/vr/business/service/impl/VRApplicantProfileLocalServiceImpl.java
@@ -15,8 +15,6 @@
 package com.fds.vr.business.service.impl;
 
 import com.fds.vr.business.action.util.ConvertFormatDate;
-import com.fds.vr.business.constant.VRKeys;
-import com.fds.vr.business.exception.NoSuchVRApplicantProfileException;
 import com.fds.vr.business.model.VRApplicantProfile;
 import com.fds.vr.business.service.base.VRApplicantProfileLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -26,10 +24,13 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.List;
+
+import org.opencps.datamgt.model.DictCollection;
+import org.opencps.datamgt.model.DictItem;
+import org.opencps.datamgt.service.DictCollectionLocalServiceUtil;
+import org.opencps.datamgt.service.DictItemLocalServiceUtil;
 
 import aQute.bnd.annotation.ProviderType;
 
@@ -37,10 +38,14 @@ import aQute.bnd.annotation.ProviderType;
  * The implementation of the vr applicant profile local service.
  *
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.fds.vr.business.service.VRApplicantProfileLocalService} interface.
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link com.fds.vr.business.service.VRApplicantProfileLocalService} interface.
  *
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
  *
  * @author khoavd
@@ -48,210 +53,65 @@ import aQute.bnd.annotation.ProviderType;
  * @see com.fds.vr.business.service.VRApplicantProfileLocalServiceUtil
  */
 @ProviderType
-public class VRApplicantProfileLocalServiceImpl
-	extends VRApplicantProfileLocalServiceBaseImpl {
+public class VRApplicantProfileLocalServiceImpl extends VRApplicantProfileLocalServiceBaseImpl {
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
-	 * Never reference this class directly. Always use {@link com.fds.vr.business.service.VRApplicantProfileLocalServiceUtil} to access the vr applicant profile local service.
+	 * Never reference this class directly. Always use {@link
+	 * com.fds.vr.business.service.VRApplicantProfileLocalServiceUtil} to access the
+	 * vr applicant profile local service.
 	 */
-	
-	public VRApplicantProfile updateApplicantProfile(LinkedHashMap<String, String> mapValues) {
-		
-		Date now = new Date();
 
-		long vrApplicantProfileId = counterLocalService.increment(VRApplicantProfile.class.getName());
-
-		VRApplicantProfile object = vrApplicantProfilePersistence.create(vrApplicantProfileId);
-
-		/// Add audit fields
-		object.setSyncDate(now);
-
-		// Add other fields
-		object.setApplicantCode(mapValues.get(""));
-		object.setApplicantName(mapValues.get(""));
-		object.setApplicantAddress(mapValues.get(""));
-		object.setApplicantPhone(mapValues.get(""));
-		object.setApplicantEmail(mapValues.get(""));
-		object.setApplicantFax(mapValues.get(""));
-		object.setApplicantRepresentative(mapValues.get(""));
-		object.setApplicantRepresentativeTitle(mapValues.get(""));
-		object.setApplicantContactName(mapValues.get(""));
-		object.setApplicantContactEmail(mapValues.get(""));
-		object.setApplicantContactPhone(mapValues.get(""));
-		object.setApplicantNationality(mapValues.get(""));
-		object.setMarkupCorporation(mapValues.get(""));
-		object.setCorporationId(mapValues.get(""));
-		object.setMarkupDesigner(mapValues.get(""));
-		object.setMarkupOverseasManufacturer(mapValues.get(""));
-		object.setMarkupDomesticsManufacturer(mapValues.get(""));
-		object.setMarkupImporter(mapValues.get(""));
-		object.setMarkupComponentXCG(mapValues.get(""));
-		object.setMarkupComponentXMY(mapValues.get(""));
-		object.setMarkupComponentXCD(mapValues.get(""));
-		object.setMarkupComponentXDD(mapValues.get(""));
-		object.setMarkupComponentXCN(mapValues.get(""));
-		object.setMarkupComponentXCH(mapValues.get(""));
-		object.setMarkupXCG(mapValues.get(""));
-		object.setMarkupXMY(mapValues.get(""));
-		object.setMarkupXCD(mapValues.get(""));
-		object.setMarkupXDD(mapValues.get(""));
-		object.setMarkupXCN(mapValues.get(""));
-		object.setMarkupXCH(mapValues.get(""));
-		object.setIssueTypeXCG(mapValues.get(""));
-		object.setIssueTypeXMY(mapValues.get(""));
-		object.setIssueTypeXCD(mapValues.get(""));
-		object.setIssueTypeXDD(mapValues.get(""));
-		object.setIssueTypeXCN(mapValues.get(""));
-		object.setIssueTypeXCH(mapValues.get(""));
-		object.setIssueTypeDescription(mapValues.get(""));
-		object.setApplicantRegion(mapValues.get(""));
-		object.setApplicantCeremonyDate(mapValues.get(""));
-		object.setApplicantOperationPeriod(mapValues.get(""));
-		object.setApplicantBusinessType(mapValues.get(""));
-		object.setApplicantMetadata(mapValues.get(""));
-		object.setApplicantStatus(mapValues.get(""));
-
-//		object.setModifyDate(DATEEEEEEE);
-
-		return vrApplicantProfilePersistence.update(object);
+	public VRApplicantProfile findByApplicantCode(String applicantCode) {
+		return vrApplicantProfilePersistence.fetchByapplicantCode(applicantCode);
 	}
-	
-	public List<VRApplicantProfile> findBymappingStatus(long mtCore, String mappingStatus)  throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findBymappingStatus(mtCore, mappingStatus);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-	
-	public List<VRApplicantProfile> findByapplicantCode(long mtCore, String applicantCode)  throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findByapplicantCode(mtCore, applicantCode);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public List<VRApplicantProfile> findBymappingMA_CTY(long mtCore, String mappingMA_CTY)  throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findBymappingMA_CTY(mtCore, mappingMA_CTY);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public List<VRApplicantProfile> findBymappingTEN_CTY(long mtCore, String mappingTEN_CTY)  throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findBymappingTEN_CTY(mtCore, mappingTEN_CTY);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public List<VRApplicantProfile> findByapplicantRegion(long mtCore, String applicantRegion) throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findByapplicantRegion(mtCore, applicantRegion);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public List<VRApplicantProfile> findByapplicantCity(long mtCore, String applicantCity) throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findByapplicantCity(mtCore, applicantCity);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public List<VRApplicantProfile> findByapplicantStatus(long mtCore, String applicantStatus) throws SystemException {
-		try {
-			return vrApplicantProfilePersistence.findByapplicantStatus(mtCore, applicantStatus);
-		} catch (Exception e) {
-			_log.error(e);
-		}
-		return new ArrayList<VRApplicantProfile>();
-		
-	}
-
-	public VRApplicantProfile findByMT_APP_CODE(long mtCore, String applicantCode) {
-		try {
-			return vrApplicantProfilePersistence.findByMT_APP_CODE(mtCore, applicantCode);
-		} catch (NoSuchVRApplicantProfileException e) {
-			return null;
-		}
-	}
-	
 
 	public JSONArray findVRAppicantProfile(String sql, List<String> columnNames, List<String> dataTypes,
 			Class<?> modelClazz, String modelClassName, int start, int end) throws SystemException {
-		
-		return vrApplicantProfileFinder.findVRAppicantProfile(sql, columnNames, dataTypes, modelClazz, modelClassName, start, end);
+
+		return vrApplicantProfileFinder.findVRAppicantProfile(sql, columnNames, dataTypes, modelClazz, modelClassName,
+				start, end);
 	}
 
 	public long countVRAppicantProfile(String sql) throws SystemException {
 
 		return vrApplicantProfileFinder.countVRAppicantProfile(sql);
 	}
-	
-	public VRApplicantProfile createVRApplicantProfile(VRApplicantProfile object)
-			throws SystemException {
 
-		long id = counterLocalService.increment(VRApplicantProfile.class.getName());
-
-		object.setId(id);
-
-		Date now = new Date();
-
-		object.setModifyDate(now);
-
-		return vrApplicantProfilePersistence.update(object);
-	}
-	
-	public VRApplicantProfile updateVRApplicantProfile(VRApplicantProfile object) throws SystemException{
-
-		Date now = new Date();
-
-		object.setModifyDate(now);
-
-		return vrApplicantProfilePersistence.update(object);
-	}
-	
 	public VRApplicantProfile adminProcessData(JSONObject objectData, long mtCore, String applicantCode) {
 		VRApplicantProfile object = null;
 
-		object = vrApplicantProfilePersistence.fetchByMT_APP_CODE(mtCore, applicantCode);
-		if(object == null) {
-			long id = counterLocalService.increment(VRApplicantProfile.class.getName());
-	
-			object = vrApplicantProfilePersistence.create(id);
-		} else {
-			String applicantStatus = object.getApplicantStatus();
-			if(applicantStatus.equals(VRKeys.CHO_DUYET) || applicantStatus.equals(VRKeys.DA_DUYET)) {
-				_log.info("Error: Khong duoc sua thong tin neu doanh nghiep dang cho duyet hoac da duyet");
-				return null;
-			}
+		object = vrApplicantProfilePersistence.fetchByapplicantCode(applicantCode);
+		if (object == null) {
+			long vrApplicantProfileId = counterLocalService.increment(VRApplicantProfile.class.getName());
+
+			object = vrApplicantProfilePersistence.create(vrApplicantProfileId);
 		}
-		
+
 		Date now = new Date();
 		object.setModifyDate(now);
 		if (objectData.getString("syncDate") != null && !StringPool.BLANK.equals(objectData.getString("syncDate"))) {
-			object.setSyncDate(ConvertFormatDate.parseStringToDate(objectData.getString("syncDate"), ConvertFormatDate._ddMMyyy_HHmm));
+			object.setSyncDate(ConvertFormatDate.parseStringToDate(objectData.getString("syncDate"),
+					ConvertFormatDate._ddMMyyy_HHmm));
 		}
-		
+
+		try {
+			DictCollection dictCollection = DictCollectionLocalServiceUtil
+					.fetchByF_dictCollectionCode("ADMINISTRATIVE_REGION", 55217L);
+			DictItem dictItem_City = DictItemLocalServiceUtil.fetchByF_dictItemCode(
+					objectData.getString("applicantCityCode"), dictCollection.getDictCollectionId(), 55217L);
+			object.setApplicantCity(dictItem_City.getItemName());
+
+			DictItem dictItem_District = DictItemLocalServiceUtil.fetchByF_dictItemCode(
+					objectData.getString("applicantDistrictCode"), dictCollection.getDictCollectionId(), 55217L);
+			object.setApplicantDistrictName(dictItem_District.getItemName());
+
+			DictItem dictItem_Ward = DictItemLocalServiceUtil.fetchByF_dictItemCode(
+					objectData.getString("applicantWardCode"), dictCollection.getDictCollectionId(), 55217L);
+			object.setApplicantWardName(dictItem_Ward.getItemName());
+		} catch (Exception e) {
+		}
+
 		object.setMtCore(objectData.getLong("mtCore"));
 		object.setMappingMA_CTY(objectData.getString("mappingMA_CTY"));
 		object.setMappingTEN_CTY(objectData.getString("mappingTEN_CTY"));
@@ -273,12 +133,9 @@ public class VRApplicantProfileLocalServiceImpl
 		object.setApplicantContactPhone(objectData.getString("applicantContactPhone"));
 		object.setApplicantNationality(objectData.getString("applicantNationality"));
 		object.setApplicantRegion(objectData.getString("applicantRegion"));
-		object.setApplicantCity(objectData.getString("applicantCity"));
 		object.setApplicantCityCode(objectData.getString("applicantCityCode"));
 		object.setApplicantDistrictCode(objectData.getString("applicantDistrictCode"));
-		object.setApplicantDistrictName(objectData.getString("applicantDistrictName"));
 		object.setApplicantWardCode(objectData.getString("applicantWardCode"));
-		object.setApplicantWardName(objectData.getString("applicantWardName"));
 		object.setApplicantRegionCode(objectData.getString("applicantRegionCode"));
 		object.setApplicantNationalityCode(objectData.getString("applicantNationalityCode"));
 		object.setMarkupCorporation(objectData.getString("markupCorporation"));
@@ -323,7 +180,7 @@ public class VRApplicantProfileLocalServiceImpl
 
 		return object;
 	}
-	
+
 	private Log _log = LogFactoryUtil.getLog(VRApplicantProfileLocalServiceImpl.class);
 
 }
